a5040924442ca7b2a366c5adb163cd611c32f1cc
Heather Johnson-Ledlie <heatherjohnson120@yahoo.com>
2019-06-09 22:13:10 -0400

    reworking

--- src/Pages/AllEmployees.js
+++ src/Pages/AllEmployees.js
@@ -14,6 +14,7 @@ export default function AllEmployees() {
         getEmployeeList(resp.data)
       })
   }
+
   useEffect(() => {
     fetchEmployees()
   }, [])
--- src/Pages/Employee.js
+++ src/Pages/Employee.js
@@ -4,6 +4,20 @@ import { Link } from 'react-router-dom'
 
 export default function Employee(props) {
   const [deletedEmployee, sendDeletedEmployee] = useState([])
+  const [selectedEmployee, showSelectedEmployee] = useState([])
+
+  const selectEmployee = () => {
+    axios
+      .put(
+        `https://sdg-staff-directory-app.herokuapp.com/api/Plix/employees/${
+          props.employee.id
+        }`
+      )
+      .then(resp => {
+        console.log({ resp })
+        showSelectedEmployee(resp.data)
+      })
+  }
 
   const removeEmployee = () => {
     axios
@@ -20,6 +34,7 @@ export default function Employee(props) {
 
   return (
     <>
+      <h1>{props.selectedEmployee}</h1>
       <section className="employee-list">
         <p>
           <span className="input">Name: </span>
@@ -31,7 +46,7 @@ export default function Employee(props) {
           {props.employee.jobTitle}
         </p>
         <Link to={`/employee/${props.employee.id}`}>
-          <button>Select</button>
+          <button onClick={selectEmployee}>Select</button>
         </Link>
         <button onClick={removeEmployee}>Delete</button>
         <hr />
